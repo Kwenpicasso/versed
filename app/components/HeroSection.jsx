b5e033f41307b14d0338d7eb9fa92d5e3d4f1ce2
kwenpicasso <milezkwen@gmail.com>
2023-11-21 21:52:15 +0100

    hero

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
-import { client } from '../utils/configSanity';
 import Image from 'next/image';
 import Button from './Button';
 import HeroImageCard from './HeroImageCard';
 
 
 
-// querying the data of the products from sanity studio
-async function getData() {
-  const query = `*[_type == 'hero']{
-    
-    'bigimage': bigimage.asset->url,
-    'thirdimage': thirdimage.asset->url,
-      bigtext,
-      smalltext
-  }`;
-  const data = await client.fetch(query);
-  return data;
 
-
-}
 
 
 const HeroSection = async () => {
-  const data = (await getData())
+  
   const url = "products";
   
   return (
@@ -32,13 +18,13 @@ const HeroSection = async () => {
       <div className='w-full h-full flex gap-3 lg:flex-col'>
         {/* hero-content */}
         <div className='w-[50%] h-full relative lg:w-full'>
-          {data.map((item) => (
+          
             <div className='w-[60%] mx-auto flex flex-col justify-center lg:text-center gap-3 mt-[10%] lg:w-full lg:mx-auto lg:items-center'>
               
-              <h1 className='uppercase text-4xl font-medium sm:text-3xl text-black'>{item.bigtext}</h1>
-              <h1 className='capitalize text-sm w-[80%] md:w-[60%] sm:w-[80%] text-black'>{item.smalltext}</h1>
+              <h1 className='uppercase text-4xl font-medium sm:text-3xl text-black'>THE FORMULA FOR AMAZING SKIN</h1>
+              <h1 className='capitalize text-sm w-[80%] md:w-[60%] sm:w-[80%] text-black'>Your Daily Skincare For All Occasions. Let Your Skin Feel Moisturized And Healthy</h1>
               <div className='w-full flex gap-4 mt-[15%]'>
-              <Image src={item.thirdimage} width={200} height={200} alt='third' className='lg:hidden'/> 
+              <Image src='/j.webp' width={200} height={200} alt='third' className='lg:hidden'/> 
              <div className='absolute right-[28%] top-[45%] lg:hidden xl:right-[10%]'>
              <Button title='shop now' border='border-black' move={url} />
              </div>
@@ -47,15 +33,32 @@ const HeroSection = async () => {
 
             </div>
            
-          ))}
+    
          
         </div>
         {/* hero-image */}
         <div className='w-[50%] h-full lg:w-full relative'>
-      {data.map((item) => (
-            <HeroImageCard item={item}/>
+      
+        <div className='w-full h-full'>
+              <Image src='/k.webp' alt='big' width={400} height={400} className='w-[70%] h-[70%] lg:w-full object-contain ' />
+              {/* button on mobile */}
+              <div className='absolute right-[28%] top-[53%] hidden lg:block'>
+             <Button title='shop now' move={url} gg='hidden' />
+             </div>
+              <div className='bg-white w-[300px] absolute h-[80px] bottom-0 right-[30%] lg:hidden opacity-60 '>
+                <div className='flex flex-col gap-3 justify-center text-xs p-[6%]'>
+                   <div className='flex justify-between items-center'>
+                    <h1 className='text-black'>NEXT</h1>
+                    <h1 className='text-black'>01/03</h1>
+                   </div>
+                   <h1 className='capitalize text-black'>gentle retinol serum</h1>
+                </div>
+
+              </div>
+              
+            </div>
            
-          ))}
+          
 
         </div>
 
